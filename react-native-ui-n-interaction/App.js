9b6e23e46b628986ca347d994a30f3d249c7e268
maffiakhateguaro <maffiakhateg@gmail.com>
2026-02-18 17:36:14 +0800

    Done with react-native-ui-n-interaction

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+// import LotsOfStyles from "./components/LotsOfStyles";
+
+// export default function App() {
+//   return <LotsOfStyles />;
+// }
+
+
+
+// import FixedDimensionsBasics from "./components/FixedDimensionsBasics";
+
+// export default function App() {
+//   return <FixedDimensionsBasics />;
+// }
+
+
+
+// import FlexDimensionsBasics from "./components/FlexDimensionsBasics";
+
+// export default function App() {
+//   return <FlexDimensionsBasics />;
+// }
+
+
+
+import PercentageDimensionsBasics from "./components/PercentageDimensionsBasics";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <PercentageDimensionsBasics />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
